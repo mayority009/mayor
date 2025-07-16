86aef9829fc98e410415600abd240edf7b62298a
mayority009 <mayority002@gmail.com>
2025-07-16 11:41:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from dist/renderer
+app.use(express.static(path.join(__dirname, 'dist/renderer')));
+
+// Route all requests to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/renderer/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
